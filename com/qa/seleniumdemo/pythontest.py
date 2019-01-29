dcbc0e4711af663a927d01cef8a125e48bd81940
Divya Bodige <divya.bodige@ciqhyd.com>
2019-01-29 11:53:59 +0530

    demo on git

--- com/qa/seleniumdemo/pythontest.py
+++ com/qa/seleniumdemo/pythontest.py
@@ -4,6 +4,6 @@ driver.get("http://www.freecrm.com")
 title = driver.title
 print(title)
 assert "#1 Free CRM software in the cloud for sales and service" in title
-driver.find_element_by_name("username").send_keys("divya")
-driver.find_element_by_name("password").send_keys("test12345")
-driver.close()
+driver.find_element_by_name("username").send_keys("divyab")
+driver.find_element_by_name("password").send_keys("test123456")
+driver.quit()
